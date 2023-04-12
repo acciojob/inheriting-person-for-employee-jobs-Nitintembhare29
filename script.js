80f2f958c8096819ab752d653d630e1b88f8cc20
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-13 00:25:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // complete this js code
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
+	this._name = name;
+	this._age = age;
 }
 Person.prototype.greet = function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
 }
 
 
 function Employee(name, age, jobTitle) {
   Person.call(this, name, age); // inherit name and age from Person
-  this.jobTitle = jobTitle;
+  this._jobTitle = jobTitle;
 }
 Employee.prototype = Object.create(Person.prototype); // inherit from Person
 Employee.prototype.constructor = Employee; // set constructor
 
 Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
 }
 
 // Do not change code below this line
